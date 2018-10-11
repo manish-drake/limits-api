4f0256f7a87f05e56421559a777e15db63382146
manish <manish@drake.in>
2018-10-11 17:01:03 +0530

    Created functions for base class

--- limits.cpp
+++ limits.cpp
@@ -1,7 +1,6 @@
 #include "limits.h"
 Limits::Limits()
 {
-
 }
 
 Limits::~Limits()
@@ -10,16 +9,105 @@ Limits::~Limits()
 
 limit Limits::getTxPwrDbW()
 {
-    return limit(18.5, 27);
+    return limit();
 }
-
-
 limit Limits::getTxPwrDroopSifDb()
 {
     return limit();
 }
-
 limit Limits::getTxPwrDroopModeSDb()
+{
+    return limit();
+}
+limit Limits::getTxFreqAccySifMHz()
+{
+    return limit();
+}
+limit Limits::getTxFreqAccyModeSMHz()
+{
+    return limit();
+}
+limit Limits::getTxPulseWdSIFUs()
+{
+    return limit();
+}
+limit Limits::getTxPulseSpSIFUs()
+{
+    return limit();
+}
+limit Limits::getTxPulseWdModeSUs()
+{
+    return limit();
+}
+limit Limits::getTxPulseSpModeSUs()
+{
+    return limit();
+}
+limit Limits::getTxDvrstIsoDB()
+{
+    return limit();
+}
+limit Limits::getRepDlySIFUs()
+{
+    return limit();
+}
+limit Limits::getRepDlySIFACUs()
+{
+    return limit();
+}
+limit Limits::getRepDlyModeSUs()
+{
+    return limit();
+}
+limit Limits::getRepJitSIFUs()
+{
+    return limit();
+}
+limit Limits::getRepJitModeS()
+{
+    return limit();
+}
+limit Limits::getRxMTLSIFDBm()
+{
+    return limit();
+}
+limit Limits::getRxMTLModeSDBm()
+{
+    return limit();
+}
+limit Limits::getRxMTLSIFACModeSDB()
+{
+    return limit();
+}
+limit Limits::getRxSLSSupSIFDB()
+{
+    return limit();
+}
+limit Limits::getRxSLSNoSupSIFDB()
+{
+    return limit();
+}
+limit Limits::getRxSLSSupModeSDB()
+{
+    return limit();
+}
+limit Limits::getRxSLSNoSupModeSDB()
+{
+    return limit();
+}
+limit Limits::getRxSLSSupAlClDB()
+{
+    return limit();
+}
+limit Limits::getRxSLSNoSupAlClDB()
+{
+    return limit();
+}
+limit Limits::getRxSLSSupATCAlClDB()
+{
+    return limit();
+}
+limit Limits::getRxSLSNoSupATCAlClDB()
 {
     return limit();
 }
--- limits.h
+++ limits.h
@@ -6,15 +6,44 @@ using std::string;
 
 class Limits
 {
-  private:
-    
-  public:
-    Limits();
-    ~Limits();
+private:
+public:
+  Limits();
+  ~Limits();
 
-    limit getTxPwrDbW();
+  virtual limit getTxPwrDbW(); //#	1
 
-    limit getTxPwrDroopSifDb();
-    virtual limit getTxPwrDroopModeSDb();
+  virtual limit getTxPwrDroopSifDb();   //#	2
+  virtual limit getTxPwrDroopModeSDb(); //#	3
+
+  virtual limit getTxFreqAccySifMHz();   //#	4
+  virtual limit getTxFreqAccyModeSMHz(); //#	5
+  virtual limit getTxPulseWdSIFUs();   //#	6
+  virtual limit getTxPulseSpSIFUs();   //#	7
+  virtual limit getTxPulseWdModeSUs(); //#	8
+  virtual limit getTxPulseSpModeSUs(); //#	9
+  virtual limit getTxDvrstIsoDB();     //#	10
+
+  virtual limit getRepDlySIFUs();   //#	11
+  virtual limit getRepDlySIFACUs(); //#	12
+  virtual limit getRepDlyModeSUs(); //#	13
+
+  virtual limit getRepJitSIFUs(); //#	14
+  virtual limit getRepJitModeS(); //#	15
+
+  virtual limit getRxMTLSIFDBm(); //#	16
+
+  virtual limit getRxMTLModeSDBm(); //#	17
+
+  virtual limit getRxMTLSIFACModeSDB(); //#	18
+
+  virtual limit getRxSLSSupSIFDB();       //#	19
+  virtual limit getRxSLSNoSupSIFDB();     //#	20
+  virtual limit getRxSLSSupModeSDB();     //#	21
+  virtual limit getRxSLSNoSupModeSDB();   //#	22
+  virtual limit getRxSLSSupAlClDB();      //#	23
+  virtual limit getRxSLSNoSupAlClDB();    //#	24
+  virtual limit getRxSLSSupATCAlClDB();   //#	25
+  virtual limit getRxSLSNoSupATCAlClDB(); //#	26
 };
-#endif //LIMITS_H   
+#endif //LIMITS_H
